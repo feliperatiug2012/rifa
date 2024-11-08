f354345a33b84c2f21ec845fd975fe4bc23f84a3
feliperatiug2012 <feliperatiug2012@gmail.com>
2024-11-07 20:33:02 -0500

    fixes

--- api/helpers/message.js
+++ api/helpers/message.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -22,29 +21,4 @@ export const sendMessage = (phone, number) => {
       .then(res => res.json())
       .then(json => console.log(json))
       .catch(err => console.error(err));
-=======
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const sendMessage = (phone, number) => {
-    const url = process.env.WS_URL;
-    const options = {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-        authorization: `Bearer ${process.env.WS_TOKEN}`
-      },
-      body: JSON.stringify({
-        chatId: `57${phone}@c.us`,
-        message: `Gracias por apoyar la rifa profondos torneo Chinauta 2024 - Club Deportivo Atletico Independiente - categoría 2013-2014-2015. Recuerda que tu número para el sorteo es el ${number}`,
-      })
-    };
-    
-    fetch(url, options)
-      .then(res => res.json())
-      .then(json => console.log(json))
-      .catch(err => console.error(err));
->>>>>>> origin/master
 }
