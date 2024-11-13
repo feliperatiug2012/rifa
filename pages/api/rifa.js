0185c0d447882db5a3fa055dbdc8c6b2a61f5f58
feliperatiug2012 <feliperatiug2012@gmail.com>
2024-11-12 19:24:28 -0500

    one more time

--- pages/api/rifa.js
+++ pages/api/rifa.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 export default function handler(req, res) {
     res.status(200).json({ text: 'Hello' });
-=======
-export default function handler(req, res) {
-    res.status(200).json({ text: 'Hello' });
->>>>>>> origin/master
 }
